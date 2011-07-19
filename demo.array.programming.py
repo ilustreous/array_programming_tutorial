66012462942cf205a99330c285e704a692712c3c
Joseph Ilustre <joseph@ilustreous.com>
2011-07-18 22:39:09 -0400

    adding demo.array

---
+++ demo.array.programming.py
@@ -0,0 +1,16 @@
+"""Introduction to array programming.
+
+Run this in your terminal
+>> $ ipython -pylab -p sh
+
+Run this in the ipython repl
+>> run ppug.ipython.demo.py
+
+"""
+
+from __future__ import with_statement
+from IPython.demo import ClearIPDemo
+
+demo_title =  "Intro to Array Programming"
+execdemo = ClearIPDemo('array.programming.py', title=demo_title) 
+
